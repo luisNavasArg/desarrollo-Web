7dace17128a230632347de6bebfa530c582ea7a8
jeesssik <jeesssik@gmail.com>
2019-08-22 18:53:40 -0300

    Clase1 variables, tipos

---
+++ js/script.js
@@ -0,0 +1,34 @@
+/*declaración de una variable
+alert();*/
+let nombre_variable = "Luis";
+// alert(nombre_variable);
+//variable string
+console.log(nombre_variable);
+//variable numerica
+let y = 5;
+//operador aritmetico
+// alert(y + 3);
+console.log("El resultado es: " + y+"3");
+//variable booleana
+let llueve=true;
+// if(llueve){
+// 	console.log("Entrá la ropa");
+// }else{
+// 	console.log("Sacamos la ropa al sol");
+// }
+// if(y<10){
+// 	console.log("se cumple la condición");
+// }else{
+// 	console.log("No se cumple la condición");
+// }
+
+let a =parseInt(prompt("Escribí un número"));
+let b =parseInt(prompt("Escribí otro número"));
+if(a < b){
+	console.log("a es menor y b es mayor");
+}else{
+	console.log("b es menor y a es mayor");
+}
+
+
+
